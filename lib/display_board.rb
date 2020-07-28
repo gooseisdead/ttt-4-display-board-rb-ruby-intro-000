cee09927f528d332e9b1a62a4591f9be6d7fa11c
Mike Smith <gooseisdead@mac.com>
2020-07-28 22:47:37 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,4 @@
-board = ["O","X"," "," ","X"," ","X","O"," "]
+board = ["X","X","X"," ","X"," ","O","O","O"]
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
